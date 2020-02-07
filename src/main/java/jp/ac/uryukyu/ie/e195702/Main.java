6dc3d73089988ba822a13b0c4128dde7625bfac7
Ryo Okuma <e195702@ie.u-ryukyu.ac.jp>
2020-02-07 15:41:11 +0900

    card作成

---
+++ src/main/java/jp/ac/uryukyu/ie/e195702/Main.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e195702;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Card main = new Card();
+        main.cards();
+
+    }
+}
